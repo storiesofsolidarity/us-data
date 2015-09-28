84b3589507c62c12967641c091a4ef3bba585c8a
Josh Levinger <josh@levinger.net>
2015-09-28 14:22:37 -0700

    split out script constants, utils

--- geography/scripts/split_places.py
+++ geography/scripts/split_places.py
@@ -1,7 +1,9 @@
-import os, sys, csv, json, collections
+import sys
+from constants import STATE_ABBR
+from utils import relative_path, load_csv_columns, split_dict_by, write_json
 
 # map colum names from gazetteer tsv to output json
-COLUMNS = {
+GAZETTEER_COLUMNS = {
     'USPS': 'state',
     'GEOID': 'fips',
     'NAME': 'name',
@@ -10,70 +12,12 @@ COLUMNS = {
 }
 
 
-# map state abbreviations to full name, for writing out
-STATES = {
-    'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California',
-    'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware', 'DC': 'District of Columbia',
-    'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois',
-    'IN': 'Indiana', 'IA': 'Iowa', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana',
-    'ME': 'Maine', 'MD': 'Maryland', 'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota',
-    'MS': 'Mississippi', 'MO': 'Missouri', 'MT': 'Montana', 'NE': 'Nebraska', 'NV': 'Nevada',
-    'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York',
-    'NC': 'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon',
-    'PA': 'Pennsylvania', 'PR': 'Puerto Rico', 'RI': 'Rhode Island', 'SC': 'South Carolina',
-    'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VT': 'Vermont',
-    'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 'WI': 'Wisconsin', 'WY': 'Wyoming'
-}
-
-
-def relative_path(fn):
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), fn))
-
-
-def converting_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
-    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
-    for row in csv_reader:
-        yield [unicode(cell, 'latin-1') for cell in row]
-
-
-def load_gazetteer(filename):
-    r = []
-    with open(filename, 'r') as f:
-        gazetteer = converting_csv_reader(f, delimiter="\t")
-        headers = next(gazetteer, None)  # parse the headers
-        columns = {}
-        for (i, h) in enumerate(headers):
-            h = h.strip()
-            if h in COLUMNS:
-                columns[i] = h
-
-        for line in gazetteer:
-            d = {}
-            for (column, index) in columns.items():
-                rename = COLUMNS[index]
-                value = line[column].strip()
-
-                if rename is 'name':
-                    # strip trailing identifier (city, town, etc)
-                    value = ' '.join(value.split(' ')[:-1])
-                d[rename] = value
-
-            r.append(d)
-        return r
-
-
-def split_states(data):
-    split_dict = collections.defaultdict(list)
-    for item in data:
-        state = item['state']
-        split_dict[state].append(item)
-    return split_dict
-
-
 def geojson_feature(place):
+    # strip place['name'] trailing identifier (city, town, etc)
+    place_name_strip = ' '.join(place['name'].split(' ')[:-1])
     return {
         "type": "Feature",
-        "id": place['name'],
+        "id": place_name_strip,
         "geometry": {
             "type": "Point",
             "coordinates": [float(place['lon']), float(place['lat'])]
@@ -82,24 +26,19 @@ def geojson_feature(place):
     }
 
 
-def write_out(filename, data):
-    with open(filename, 'w') as f:
-        f.write(json.dumps(data))
-
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         fn = sys.argv[1]
     else:
         fn = relative_path("../raw/Gaz_places_national.txt")
     try:
-        gazeteer = load_gazetteer(fn)
+        gazetteer = load_csv_columns(fn, GAZETTEER_COLUMNS, delimiter='\t')
     except IOError:
         print "unable to load", fn
         sys.exit(-1)
 
-    for (abbr, data) in split_states(gazeteer).items():
-        state_name = STATES[abbr].replace(' ', '_')
+    for (abbr, data) in split_dict_by(gazetteer, 'state').items():
+        state_name = STATE_ABBR[abbr].replace(' ', '_')
 
         geojson_collection = {
             "type": "FeatureCollection",
@@ -111,4 +50,4 @@ if __name__ == "__main__":
 
         print "writing %d places in %s" % (len(data), state_name)
         out_fn = relative_path('../places/%s.geo.json' % state_name)
-        write_out(out_fn, geojson_collection)
+        write_json(out_fn, geojson_collection)
---
+++ geography/scripts/utils.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import csv
+import json
+import collections
+
+
+def relative_path(fn):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), fn))
+
+
+def csv_reader_converter(utf8_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        yield [unicode(cell, 'latin-1') for cell in row]
+
+
+def load_csv_columns(filename, column_names=None, delimiter=','):
+    r = []
+    print filename
+    with open(filename, 'r') as f:
+        data_file = csv_reader_converter(f, delimiter=delimiter, quoting=csv.QUOTE_NONE)
+        headers = next(data_file, None)  # parse the headers
+        columns = {}
+        for (i, h) in enumerate(headers):
+            h = h.strip()
+            if h in column_names or not column_names:
+                columns[i] = h
+
+        for line in data_file:
+            d = {}
+            for (column, index) in columns.items():
+                rename = column_names[index]
+                value = line[column].strip()
+                d[rename] = value
+            r.append(d)
+        return r
+
+
+def split_dict_by(data, key):
+    split_dict = collections.defaultdict(list)
+    for item in data:
+        desired_val = item[key]
+        split_dict[desired_val].append(item)
+    return split_dict
+
+
+def read_json(filename):
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
+
+
+def write_json(filename, data):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(data))
